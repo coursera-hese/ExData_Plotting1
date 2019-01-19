0146a0a5f82c36f3c9128744cd4a6625fb2446e3
hese <helgar@gmx.at>
2019-01-19 16:09:50 +0100

    getting_data.R

--- getting_data.R
+++ getting_data.R
@@ -4,15 +4,14 @@
 # cell estimated:         8 bytes
 print(paste("estimated memory size:", round(2075259 * 9 * 8 / 1024 / 1024), "MB"))
 
-## Getting & processing data
+# Getting data
 f <- "household_power_consumption.zip"
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = f)
 unzip(f)
 df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
-# Convert date column
+# Processing data
 library(dplyr)
-df <- df %>% dplyr::mutate(Date = as.Date(Date, format = "%d/%m/%Y"))
-# filter data-set for 2007-02-01 and 2007-02-02
-targetDates <- { df$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")) }
-df <- df[targetDates,]
+df <- df %>% 
+    mutate(datetime = as.POSIXct( strptime( paste(Date, Time), format = "%d/%m/%Y %H:%M:%S") ) ) %>% 
+    filter(datetime >= as.Date("2007-02-01") & datetime < as.Date("2007-02-03"))
