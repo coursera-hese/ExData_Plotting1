9b62bdb16d4af91c21d5554acf29702a0c342084
hese <helgar@gmx.at>
2019-01-19 15:43:11 +0100

    plot3

--- plot3.R
+++ plot3.R
@@ -2,24 +2,29 @@
 # plot3
 #
 
-# Getting & processing data
+# Getting data
 f <- "household_power_consumption.zip"
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = f)
 unzip(f)
 df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
-
+# Processing data
 library(dplyr)
-# Convert date column
-df <- df %>% dplyr::mutate(Date = as.Date(Date, format = "%d/%m/%Y"))
-# filter Date for '2007-02-01' and '2007-02-02'
-targetDates <- { df$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")) }
-df <- df[targetDates,]
+df <- df %>% 
+    mutate(datetime = as.POSIXct( strptime( paste(Date, Time), format = "%d/%m/%Y %H:%M:%S") ) ) %>% 
+    filter(datetime >= as.Date("2007-02-01") & datetime < as.Date("2007-02-03"))
 
 #
 # Plotting
 #
+png("plot3.png")  ## Instead of `dev.copy`
 
-# TODO
+with(df, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(df, lines(datetime, Sub_metering_2, type = "l", col = "red"))
+with(df, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", pch = 1, col = c("black", "red", "blue"), legend = names(df[7:9]))
+#legend("topright", col = c("black", "red", "blue"), legend = c("", "", ""))
 
-dev.copy(png, file = "plot3.png")  ## Copy my plot to a PNG file
+## HINT: When copying plots from screen to file with `dev.copy`, it cuts off the legend! 
+## Use `png` instead to write plot directly to a file!
+#dev.copy(png, file = "plot3.png")  
 dev.off()   ## Don't forget to close the PNG device!
